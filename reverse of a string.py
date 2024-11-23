2f68011d5fbce8a02835caa885d98f1178fd1c22
Dileep kumar <gembalidileepkumar10@gmail.com>
2024-11-23 15:25:45 +0530

    I changed the reverse of a string code to short form

--- reverse of a string.py
+++ reverse of a string.py
@@ -1,10 +1,6 @@
 # method1
 #reverse of the string using simple iterations
 num=int(input())
-dummy=num
-reverse=0
-while num>0:
-  remainder=num%10
-  reverse=(reverse*10)+remainder
-  num=num//10
-print(f"revers of a number is {reverse}")
+x=str(num)
+x=x[::-1]
+print(f"Reverse of a number",x)
